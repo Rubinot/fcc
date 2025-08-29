524afd307bf5980d28fb63ac6ec8ca751b428d63
Rubin <98098140+Rubinot@users.noreply.github.com>
2025-08-29 14:39:50 +0545

    Add files via upload

---
+++ if_01.py
@@ -0,0 +1,6 @@
+x=5
+if x<10:
+	print('smaller')
+if x>10:
+	print('larger')
+	print('program ended')
---
+++ if_02.py
@@ -0,0 +1,7 @@
+a = input('enter id')
+b = input('enter passkey')
+x = a+b
+if x== 'rubion':
+ print('welcome')
+else:
+	print('reenter')
